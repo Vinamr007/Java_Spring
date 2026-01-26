a51c40ed801a5190bea0315b79bb7579f0d597bd
Vinamr Naik <vinamrnaik206@gmail.com>
2026-01-26 12:29:07 +0530

    http methods

--- first/src/main/java/com/demo/first/app/UserController.java
+++ first/src/main/java/com/demo/first/app/UserController.java
@@ -1,25 +1,43 @@
 package com.demo.first.app;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    private Map<Integer, User> userdb =new HashMap<>();
+    private Map<Integer, User> userdb = new HashMap<>();
 
     @PostMapping
-public String createUser(@RequestBody User user){
+    public String createUser(@RequestBody User user) {
         System.out.println(user.getEmail());
-    userdb.put(user.getId(), user);
-    return "user created successfully";
-}
+        userdb.putIfAbsent(user.getId(), user);
+        return "user created successfully";
+    }
+
+    @PutMapping
+    public String updateUser(@RequestBody User user) {
+        if (userdb.containsKey(user.getId()))
+            userdb.put(user.getId(), user);
+            return "update successful";
 
 
+    }
 
+    @DeleteMapping("/{id}")
+    public String deleteUsser(@PathVariable int id){
+        if(!userdb.containsKey(id))
+            return "user not found";
+        userdb.remove(id);
+        return "delete successful";
+    }
+
+    @GetMapping
+    public List<User> getUsers(){
+        return new ArrayList<>(userdb.values());
+    }
 }
